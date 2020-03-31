f145e83f54abb97bda322f984d2038100ef2cde9
linhbui98 <linhbuiquang2321998@gmail.com>
2020-03-31 17:41:33 +0700

    test route

---
+++ pages/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <form>
+      <label>Login</label>
+      <input type="text" placeholder="username" value="linh" />
+      <input type="password" value="123" />
+      <nuxt-link to="/">Home</nuxt-link>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend()
+</script>
---
+++ pages/staff/profile/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/staff">Staff</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend()
+</script>
